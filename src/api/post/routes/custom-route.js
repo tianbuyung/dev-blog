9318e7a40caa39e8b563251a0f2e5442bd183473
Septian Maulana <septianm028@gmail.com>
2023-07-27 16:14:33 +0700

    feat: customize core route for tags and create custom route for post

---
+++ src/api/post/routes/custom-route.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   routes: [
+//     {
+//       method: "GET",
+//       path: "/posts/example",
+//       handler: "myCustomController.example",
+//       config: {
+//         find: {
+//           auth: false, // disabling the Strapi JWT auth system for this route
+//           policies: [],
+//           middlewares: [],
+//         },
+//         findOne: {},
+//         create: {},
+//         update: {},
+//         delete: {},
+//       },
+//     },
+//   ],
+// };
--- src/api/tag/routes/tag.js
+++ src/api/tag/routes/tag.js
@@ -1,9 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * tag router
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::tag.tag');
+module.exports = createCoreRouter(
+  "api::tag.tag"
+  // {
+  //   prefix: "", // test -> /test/tags
+  //   only: ["find", "findOne"], // include methods
+  //   except: [], // exclude methods
+  //   config: {
+  //     find: {
+  //       auth: false, // disabling the Strapi JWT auth system for this route
+  //       policies: [],
+  //       middlewares: [],
+  //     },
+  //     findOne: {},
+  //     create: {},
+  //     update: {},
+  //     delete: {},
+  //   },
+  // }
+);
