56008e844bfdf074e0abfb3d8bd0933ea63e88cf
Septian Maulana <septianm028@gmail.com>
2023-08-04 14:36:25 +0700

    feat: create custom policies for post

---
+++ src/api/post/policies/check-role.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * `check-role` policy
+ */
+
+module.exports = (policyContext, config, { strapi }) => {
+  const { userRole } = config;
+  const isEligible =
+    policyContext.state.user && policyContext.state.user.role.name === userRole;
+  strapi.log.info(`User role: ${isEligible}`);
+  // Add your own logic here.
+  strapi.log.info("In is-admin policy.");
+
+  if (isEligible) {
+    return true;
+  }
+
+  return false;
+};
