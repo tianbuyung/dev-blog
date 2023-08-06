d2b57c6607f07f5a94b249bca17791a16fc6a747
Septian Maulana <septianm028@gmail.com>
2023-08-06 09:33:17 +0700

    feat: create extending core controller

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -15,4 +15,23 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
       ctx.body = err;
     }
   },
+
+  // Method 2: Wrapping a core action (leaves core logic in place)
+  async find(ctx) {
+    // some custom logic here
+    ctx.query = { ...ctx.query, local: "en" }; // add more query /posts?publicationState=preview&local=en
+    console.log(ctx.query);
+    /**
+     * {publicationState: "preview"}
+     * {publicationState: "preview", local: "en"}
+     */
+
+    // Calling the default core action
+    const { data, meta } = await super.find(ctx);
+
+    // some more custom logic
+    meta.date = Date.now();
+
+    return { data, meta };
+  },
 }));
