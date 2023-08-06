40eb1f1b2e28e9274e1e8426c89086bf92b9e8fd
Septian Maulana <septianm028@gmail.com>
2023-08-06 09:17:52 +0700

    feat: create custom controller example action

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 
 /**
  * post controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController("api::post.post", ({ strapi }) => ({
+  // Method 1: Creating an entirely custom action
+  async exampleAction(ctx) {
+    try {
+      ctx.body = "ok";
+    } catch (err) {
+      ctx.body = err;
+    }
+  },
+}));
