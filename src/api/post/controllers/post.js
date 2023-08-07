1dc7f4de35811a9f34d664dcb04af62cc1685f64
Septian Maulana <septianm028@gmail.com>
2023-08-07 09:25:53 +0700

    feat: create premium post controller for solution 2: better approach

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -20,21 +20,50 @@ module.exports = createCoreController("api::post.post", ({ strapi }) => ({
   },
 
   // Method 2: Wrapping a core action (leaves core logic in place)
-  async find(ctx) {
-    // 1. Premium posts solution 1: the worst because you need more logic in your controller, we need avoid this approach
-    // fetch all posts (including premium posts)
-    const { data, meta } = await super.find(ctx);
+  // async find(ctx) {
+  //   // 1. Premium posts solution 1: the simple but the worst because you need more logic in your controller, we need avoid this approach
+  //   // fetch all posts (including premium posts)
+  //   const { data, meta } = await super.find(ctx);
 
-    if (ctx.state.user) {
+  //   if (ctx.state.user) {
+  //     return { data, meta };
+  //   }
+
+  //   // Not authenticated
+  //   const filteredData = data.filter((post) => {
+  //     return !post.attributes.premium;
+  //   }); // filter preferably in the service and making big query for public request without premium posts
+
+  //   return { data: filteredData, meta };
+  // },
+
+  // 2. Premium posts solution 2: rewrite the action to fetch only needed posts
+  async find(ctx) {
+    // if the request is authenticated
+    const isRequestingNonPremium =
+      ctx.query.filters && ctx.query.filters.premium["eq"] === false;
+
+    if (ctx.state.user || isRequestingNonPremium) {
+      const { data, meta } = await super.find(ctx);
       return { data, meta };
     }
 
-    // Not authenticated
-    const filteredData = data.filter((post) => {
-      return !post.attributes.premium;
-    }); // filter preferably in the service and making big query for public request without premium posts
+    // if the request is not authenticated (public request)
+    // let's call the underlying service with an additional filter param: premium === false
+    // /posts?filters[premium]=false
+    const { query } = ctx;
 
-    return { data: filteredData, meta };
+    const filteredPosts = await strapi.service("api::post.post").find({
+      ...query,
+      filters: {
+        ...query.filters,
+        premium: false,
+      },
+    });
+
+    const sanitizedPosts = await this.sanitizeOutput(filteredPosts, ctx);
+
+    return this.transformResponse(sanitizedPosts);
   },
 
   // Method 3: Replacing a core action with proper sanitization
