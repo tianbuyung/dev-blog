9ec588d5cbeed683ff0f9f6cf65e2d0375e9d27f
Septian Maulana <septianm028@gmail.com>
2023-08-15 17:19:34 +0700

    feat: create programmatic hooks for author and user admin after create and after update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   /**
@@ -16,5 +16,55 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap({ strapi }) {
+    // we listen to lifecycle events
+    strapi.db.lifecycles.subscribe({
+      models: ["admin::user"], // only listen to events for this model
+      afterCreate: async ({ result }) => {
+        // create an author instance from the fields of the admin user
+        // that has just been created
+        // extract the fields from the newly created admin user
+        const { id, firstname, lastname, email, createdAt, updatedAt } = result;
+
+        await strapi.service("api::author.author").create({
+          data: {
+            firstname,
+            lastname,
+            email,
+            createdAt,
+            updatedAt,
+            admin_user: [id],
+          },
+        });
+      },
+      afterUpdate: async ({ result }) => {
+        // get the author that corresponds
+        // to the admin user that has just been updated
+        const correspondingAuthor = (
+          await strapi.entityService.findMany("api::author.author", {
+            populate: ["admin_user"],
+            filters: {
+              admin_user: {
+                id: result.id,
+              },
+            },
+          })
+        )[0];
+
+        // update the Author accordingly
+        const { firstname, lastname, email, username, updatedAt } = result;
+        await strapi
+          .service("api::author.author")
+          .update(correspondingAuthor.id, {
+            data: {
+              firstname,
+              lastname,
+              email,
+              username,
+              updatedAt,
+            },
+          });
+      },
+    });
+  },
 };
