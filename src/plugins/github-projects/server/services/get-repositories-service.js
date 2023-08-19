dc140d12d72fb35e45cc7563ec7b305189049a88
Septian Maulana <septianm028@gmail.com>
2023-08-19 12:31:59 +0700

    feat: fetching related projects; fix: fix 404 error during readme fetch;

--- src/plugins/github-projects/server/services/get-repositories-service.js
+++ src/plugins/github-projects/server/services/get-repositories-service.js
@@ -4,6 +4,23 @@ const { request } = require("@octokit/request");
 const axios = require("axios");
 
 module.exports = ({ strapi }) => ({
+  getProjectForRepository: async (repository) => {
+    const { id } = repository;
+
+    const matchingProjects = await strapi.entityService.findMany(
+      "plugin::github-projects.project",
+      {
+        filters: {
+          repositoryId: id,
+        },
+      }
+    );
+
+    if (matchingProjects.length === 1) return matchingProjects[0].id;
+
+    return null;
+  },
+
   getPublicRepositories: async () => {
     // Following GitHub docs formatting:
     // https://developer.github.com/v3/repos/#list-organization-repositories
@@ -22,14 +39,35 @@ module.exports = ({ strapi }) => ({
         const { id, name, description, html_url, owner, default_branch } = item;
         const readmeUrl = `https://raw.githubusercontent.com/${owner.login}/${name}/${default_branch}/README.md`;
 
-        const longDescription = (await axios.get(readmeUrl))?.data;
+        let readmeResponse;
 
-        return {
+        try {
+          const response = await axios.get(readmeUrl);
+          console.log("response");
+
+          if (response) readmeResponse = response.data;
+        } catch (error) {
+          console.log(`Error occurred on item ${name}`);
+          console.log("Error fetching ", readmeUrl);
+        }
+
+        const repositories = {
           id,
           name,
           shortDescription: description,
           url: html_url,
-          longDescription,
+          longDescription: readmeResponse || "",
+        };
+
+        // Add some logic to search for an existing project for the current repository.
+        const relatedProjectId = await strapi
+          .plugin("github-projects")
+          .service("getRepositoriesService")
+          .getProjectForRepository(repositories);
+
+        return {
+          ...repositories,
+          projectId: relatedProjectId,
         };
       })
     );
