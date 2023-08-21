688ea45b94fedce464b0e7d08ae23a1d141072cf
Septian Maulana <septianm028@gmail.com>
2023-08-21 10:39:41 +0700

    feat: create first component in admin panel

--- src/plugins/github-projects/admin/src/pages/HomePage/index.js
+++ src/plugins/github-projects/admin/src/pages/HomePage/index.js
@@ -4,15 +4,14 @@
  *
  */
 
-import React from 'react';
+import React from "react";
 // import PropTypes from 'prop-types';
-import pluginId from '../../pluginId';
+import Repo from "../../components/Repo";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>{pluginId}&apos;s HomePage</h1>
-      <p>Happy coding</p>
+      <Repo />
     </div>
   );
 };
