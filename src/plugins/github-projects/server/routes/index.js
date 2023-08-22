1117ca6caaf09a735fcb4846d4934d62873fc7cf
Septian Maulana <septianm028@gmail.com>
2023-08-22 10:47:35 +0700

    chore: update route policies in github projects plugin

--- src/plugins/github-projects/server/routes/index.js
+++ src/plugins/github-projects/server/routes/index.js
@@ -4,8 +4,8 @@ module.exports = [
     path: "/repositories", // localhost:1337/github-projects/repositories
     handler: "getRepositoriesController.index",
     config: {
-      policies: [],
-      auth: false, // temporarily disabled and TODO: change this authorized only for admin panel users
+      policies: ["admin::isAuthenticatedAdmin"],
+      // auth: false, // temporarily disabled and TODO: change this authorized only for admin panel users
     },
   },
 ];
